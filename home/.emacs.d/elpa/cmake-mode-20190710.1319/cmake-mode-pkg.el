dc203ab88203dd4d3574b131353f184802374d15
Innokentiy Alaytsev <alaitsev@gmail.com>
2019-07-13 13:41:54 +0000

    Updates all packages to the latest versions
    Signed-off-by: Innokentiy Alaytsev <alaitsev@gmail.com>

---
+++ home/.emacs.d/elpa/cmake-mode-20190710.1319/cmake-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "cmake-mode" "20190710.1319" "major-mode for editing CMake sources" '((emacs "24.1")) :stars '(#("2303" 0 4 (font-lock-face paradox-star-face))) :commit "82a4e07f8fa3cfd745218ef56a1c9a46087a0be8")
